9075cb1092897ebc556aa152f5c22098c2df01b3
Simona735 <richterova735@gmail.com>
2022-02-18 15:24:59 +0100

    style: remove unnecessary comments

--- cvicenie 1_1.py
+++ cvicenie 1_1.py
@@ -1,4 +1,3 @@
-# nacitame modul 'ppds', v ktorom mame definovanu triedu 'Thread'
 from fei.ppds import *
 
 
@@ -17,20 +16,11 @@ class Shared():
         self.array = [0] * size
 
 
-# definicia funkcie vlakna
 def fnc_test(shared):
     pass
 
 
-# vytvorenie instancie triedy 'Shared'
 shared = Shared(1_000_000)
-
-# do 't1' ulozime identifikator pracovneho vlakna
-# prvy argument pri vytvarani objektu typu 'Thread' je funkcia, ktoru ma
-# vlakno vykonavat
-# dalsie argumenty sa predaju funkcii, ktora je definovana prvym argumentom
 t1 = Thread(fnc_test, shared)
-
-# pockame na dokoncenie behu vlakna
 t1.join()
 
