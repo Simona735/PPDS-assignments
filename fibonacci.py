7dc897ea7fc6dc1e74d1141af7bfdbb3c6f5b1f1
Simona735 <richterova735@gmail.com>
2022-02-27 15:58:50 +0100

    style: typo in ads_list

--- fibonacci.py
+++ fibonacci.py
@@ -9,7 +9,7 @@ from random import randint
 from fei.ppds import Thread, Mutex, Semaphore, Event
 
 
-def compute_fibonacci(i, ads_list):
+def compute_fibonacci(i, adt_list):
     sleep(randint(1, 10)/10)
     fibonacci_seq[i + 2] = fibonacci_seq[i + 1] + fibonacci_seq[i]
 
@@ -19,12 +19,12 @@ THREADS_NUM = 10
 fibonacci_seq = [0] * (THREADS_NUM + 2)
 fibonacci_seq[1] = 1
 
-ads_list = [Semaphore(0) for i in range(THREADS_NUM)]
-ads_list[0].signal()
+adt_list = [Semaphore(0) for i in range(THREADS_NUM)]
+adt_list[0].signal()
 
 threads = list()
 for i in range(THREADS_NUM):
-    t = Thread(compute_fibonacci, i, ads_list)
+    t = Thread(compute_fibonacci, i, adt_list)
     threads.append(t)
 
 for t in threads:
