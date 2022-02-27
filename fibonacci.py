c73bc4f3e2b66adc491ce118bb5a5bd295ae028b
Simona735 <richterova735@gmail.com>
2022-02-27 15:19:03 +0100

    chore: add codebase of a program

---
+++ fibonacci.py
@@ -0,0 +1,42 @@
+"""
+2022 Simona Richterova.
+
+This module computes fibonacci sequence using various synchronization ADT.
+"""
+
+from time import sleep
+from random import randint
+from fei.ppds import Thread, Mutex, Semaphore, Event
+
+
+class AdtSemaphore:
+    """
+    Adt Semaphore object.
+
+    Args:
+        
+    """
+        
+    def __init__(self):
+        pass
+
+
+def compute_fibonacci(i):
+    sleep(randint(1, 10)/10)
+    fibonacci_seq[i + 2] = fibonacci_seq[i + 1] + fibonacci_seq[i]
+
+
+THREADS_NUM = 10
+
+fibonacci_seq = [0] * (THREADS_NUM + 2)
+fibonacci_seq[1] = 1
+
+threads = list()
+for i in range(THREADS_NUM):
+    t = Thread(compute_fibonacci, i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+print(fibonacci_seq)
