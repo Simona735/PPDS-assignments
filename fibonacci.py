e7a70eecb88e9d4585b624763282383cb5b5cf4a
Simona735 <richterova735@gmail.com>
2022-02-28 08:54:09 +0100

    refactor: add main module

--- fibonacci.py
+++ fibonacci.py
@@ -4,12 +4,13 @@
 This module computes fibonacci sequence using various synchronization ADT.
 """
 
+
 from time import sleep
 from random import randint
 from fei.ppds import Thread, Mutex, Semaphore, Event
 
 
-def compute_fibonacci(i, adt_list):
+def compute_fibonacci(i, adt_list, fibonacci_seq):
     """
     Computes fibonacci sequence using multiple threads and sync mechanism.
 
@@ -23,18 +24,22 @@ def compute_fibonacci(i, adt_list):
         adt_list[i + 1].signal()
 
 
-THREADS_NUM = 10
+def main():
+    THREADS_NUM = 10
 
-fibonacci_seq = [0] * (THREADS_NUM + 2)
-fibonacci_seq[1] = 1
+    fibonacci_seq = [0] * (THREADS_NUM + 2)
+    fibonacci_seq[1] = 1
 
-# adt_list = [Semaphore(0) for i in range(THREADS_NUM)]
-adt_list = [Event() for i in range(THREADS_NUM)]
-adt_list[0].signal()
+    # adt_list = [Semaphore(0) for i in range(THREADS_NUM)]
+    adt_list = [Event() for i in range(THREADS_NUM)]
+    adt_list[0].signal()
 
-threads = [Thread(compute_fibonacci, i, adt_list)
-           for i in range(THREADS_NUM)]
+    threads = [Thread(compute_fibonacci, i, adt_list, fibonacci_seq)
+               for i in range(THREADS_NUM)]
+    [t.join() for t in threads]
 
-[t.join() for t in threads]
+    print(fibonacci_seq)
 
-print(fibonacci_seq)
+
+if __name__ == '__main__':
+    main()
