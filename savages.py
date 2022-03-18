456618f11fb282cad93fd99880fba9bdcf173ae0
Simona735 <richterova735@gmail.com>
2022-03-18 15:38:33 +0100

    chore: add savages file

---
+++ savages.py
@@ -0,0 +1,19 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module implements the dining savages problem and it's solution.
+"""
+
+
+from time import *
+from random import randint
+from fei.ppds import Thread, Semaphore, Mutex, Event, print
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
