4f051d8dd91bb8f47e173604358f1e14cadde143
Simona735 <richterova735@gmail.com>
2022-03-23 14:47:36 +0100

    chore: add h2o.py file

---
+++ h2o.py
@@ -0,0 +1,19 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module implements The Building H2O Problem and it's solution.
+"""
+
+
+from time import *
+from random import randint, choice, shuffle
+from fei.ppds import Thread, Semaphore, print
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
