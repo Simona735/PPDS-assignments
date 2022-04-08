6d4486b5443d57101598889fe3217f359f32619e
Simona735 <richterova735@gmail.com>
2022-04-08 11:19:06 +0200

    chore: add codebase

---
+++ coroutines.py
@@ -0,0 +1,18 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module implements an example of 3 generators and a scheduler.
+"""
+
+
+from random import randint
+from time import sleep
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
