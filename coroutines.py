4b2479aecb87f8c70467a9c218fc19eed7f845d6
Simona735 <richterova735@gmail.com>
2022-04-08 11:19:36 +0200

    feat(scheduler): add scheduler object

--- coroutines.py
+++ coroutines.py
@@ -10,6 +10,30 @@ from random import randint
 from time import sleep
 
 
+class Scheduler(object):
+    """
+    Scheduler object. It is used to add generators to Scheduler object
+    and loop over them infinitely. It also closes the generator when it
+    raises StopIteration exception.
+    """
+    def __init__(self):
+        """
+        Initialize a Scheduler with a list and index set on 0.
+        """
+        self.tasks = []
+        self.index = 0
+
+    def new(self, task):
+        """
+        Add new task to Scheduler.
+
+        Args:
+            task(Generator): generator to be added to Scheduler.
+        """
+        next(task)
+        self.tasks.append(task)
+
+
 def main():
     pass
 
