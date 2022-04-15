1bf2fdbc09919ac3575f1de08bc1edffcda5f4c2
Simona735 <richterova735@gmail.com>
2022-04-15 19:54:54 +0200

    chore: add codebase

---
+++ synchronous.py
@@ -0,0 +1,18 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module implements a simple example of synchronous coroutine.
+"""
+
+
+import time
+from queue import Queue
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
