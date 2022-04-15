0c905cf7fbecbd056b79a78792206bbeb3948929
Simona735 <richterova735@gmail.com>
2022-04-15 19:58:20 +0200

    chore: add codebase

---
+++ asynchronous.py
@@ -0,0 +1,18 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module implements a simple example of asynchronous coroutine.
+"""
+
+
+import time
+import asyncio
+
+
+async def main():
+    pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
