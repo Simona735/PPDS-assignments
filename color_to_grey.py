48c68346f6349fc873c2397ece6d8c197e67e92d
Simona735 <richterova735@gmail.com>
2022-04-25 12:18:42 +0200

    feat(convert): add converting method

--- color_to_grey.py
+++ color_to_grey.py
@@ -8,6 +8,25 @@ using the GPU and the numba library.
 
 
 from numba import cuda
+import numpy as np
+import cv2
+import math
+
+
+@cuda.jit
+def color_to_grey(color, grey):
+    """
+    Convert color image to grey using cuda.
+
+    Args:
+        color(numpy array): color image with 3 dimensions (RGB colors)
+        grey(numpy array): resulting grey image with 2 dimensions
+    """
+    x, y = cuda.grid(2)
+    x_max, y_max, _ = color.shape
+    if x < x_max and y < y_max:
+        colors_sum = color[x][y][0] + color[x][y][1] + color[x][y][2]
+        grey[x][y] = colors_sum // 3
 
 
 def main():
