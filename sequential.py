2f153bccb972fd651fa021c18f8fa3011f5d84fe
Simona735 <richterova735@gmail.com>
2022-05-05 08:28:15 +0200

    chore: add codebase

---
+++ sequential.py
@@ -0,0 +1,23 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module converts 3 color images to black and white
+using the GPU and the numba library. This version of the program 
+demonstrates the operations in sequence in a single stream.
+"""
+
+
+from numba import cuda
+import cv2
+import math
+from time import perf_counter
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ streams_and_events.py
@@ -0,0 +1,24 @@
+"""
+Copyright 2022 Simona Richterova.
+Licensed to MIT https://spdx.org/licenses/MIT.html
+
+This module converts 3 color images to black and white
+using the GPU and the numba library. In addition, the program
+is optimised with the use of multiple streams. It also uses
+events to measure the computation time.
+"""
+
+
+from numba import cuda
+import cv2
+import math
+from time import perf_counter
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
